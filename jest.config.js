25b9b6554da1681e22263ad06faf4777a70cb57d
Milton Arenas <hefziben@gmail.com>
2023-12-21 14:07:24 -0500

    update setupFilesAfterEnv

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: 'jest-preset-angular',
   globalSetup: 'jest-preset-angular/global-setup',
-  setupFilesAfterEnv: ['<rootDir>/src/app/setup-jest.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
 };
