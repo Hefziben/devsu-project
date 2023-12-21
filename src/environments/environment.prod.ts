2015bd4ff65ac298b5214a0d5e32ec3b474846cf
Milton Arenas <hefziben@gmail.com>
2023-12-21 17:15:16 -0500

    update env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  authorId: '198421',
+  baseUrl: 'https://tribu-ti-staffing-desarrollo-afangwbmcrhucqfh.z01.azurefd.net/ipf-msa-productosfinancieros/bp/products'
 };
