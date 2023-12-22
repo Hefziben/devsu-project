3a59ee65599bcf25da6649b68590f03d6c6bdfd1
Milton Arenas <hefziben@gmail.com>
2023-12-21 19:40:24 -0500

    F2 added search functionality to product list

--- src/app/product/components/product-list/product-list.component.ts
+++ src/app/product/components/product-list/product-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
-import { Observable, takeUntil } from 'rxjs';
+import { Observable, Subject, debounceTime, distinctUntilChanged, switchMap, takeUntil } from 'rxjs';
 import { Product } from '../../../models/product.model';
 import { Destroyable } from '../../../shared/abstract/destroyable';
 import { GetProducts, GetProductsReset } from '../../../store/product.actions';
 import { ProductState } from '../../../store/product.store';
+import { filterItems } from 'src/app/util/product.util';
 
 @Component({
   selector: 'app-product-list',
@@ -19,6 +20,10 @@ export class ProductListComponent extends Destroyable implements OnInit {
   currentPage: number = 1;
   totalPages: number = 0;
   displayedColumns: string[] = ['Logo','Nombre del producto','Descripción','Fecha Liberación','Fecha Restructuración'];
+  searchTerm = '';
+  productFilter$ = new Subject<string>();
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   constructor(private store: Store) {
     super();
   }
@@ -26,11 +31,23 @@ export class ProductListComponent extends Destroyable implements OnInit {
   ngOnInit(): void {
     this.store.dispatch((new GetProductsReset()));
     this.products$.pipe(takeUntil(this.destroy$)).subscribe(products => {
-      this.totalItems = products.length;
-      this.totalPages = Math.ceil(this.totalItems/ this.itemsPerPage);
+      this.products = products;
+      this.filteredProducts = products;
+      this.updateTotalItems()
     })
     this.store.dispatch((new GetProducts()));
-  }
+    
+        this.productFilter$
+        .pipe(
+          debounceTime(200),
+          distinctUntilChanged(),
+          switchMap(term => filterItems(term, this.products))
+        )
+        .subscribe(filteredProducts => {
+          this.filteredProducts = filteredProducts;
+          this.updateTotalItems()
+        });
+    }
 
   getStartIndex(): number {
     return (this.currentPage - 1) * this.itemsPerPage;
@@ -47,6 +64,15 @@ export class ProductListComponent extends Destroyable implements OnInit {
   onItemsPerPageChange(itemsPerPage: number): void {
     this.itemsPerPage = itemsPerPage;
     this.currentPage = 1;
+    this.totalPages = Math.ceil(this.totalItems / this.itemsPerPage);
+  }
+
+  onSearch(): void {
+    this.productFilter$.next(this.searchTerm);
+  }
+
+  updateTotalItems() {
+    this.totalItems = this.filteredProducts.length;
     this.totalPages = Math.ceil(this.totalItems / this.itemsPerPage);
   }
 
