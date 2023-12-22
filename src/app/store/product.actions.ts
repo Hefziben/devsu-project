1b442372d9c3b881db3eef85e510205ea5038413
Milton Arenas <hefziben@gmail.com>
2023-12-22 12:35:56 -0500

    features Add product - added actions

--- src/app/store/product.actions.ts
+++ src/app/store/product.actions.ts
@@ -3,7 +3,10 @@ import { Product } from "../models/product.model";
 export interface ProductStateModel {
   products: Product[];
   loadingProducts: boolean;
+  validatingProductId: boolean;
+  productIdExist: boolean;
   loadingProductsError: string | null;
+  addingProduct: boolean;
 }
 
 // Actions
@@ -24,3 +27,38 @@ export class GetProductsFailed {
 export class GetProductsReset {
   static readonly type = '[Product] Get Products Reset';
 }
+
+export class ValidateProductId {
+  static readonly type = '[Product] Validate Products ID';
+  constructor(public ProductId: string) {}
+}
+
+export class ValidateProductIdSuccess {
+  static readonly type = '[Product] Validate Products ID Success';
+  constructor(public productIdExist: boolean) {}
+}
+
+export class ValidateProductIdFailed {
+  static readonly type = '[Product] Validate Products ID Failed';
+  constructor(public message: string) {}
+}
+
+export class AddProduct {
+  static readonly type = '[Product] Add Product';
+  constructor(public product: Product) {}
+}
+
+export class AddProductSuccess {
+  static readonly type = '[Product] Add Product Success';
+  constructor(public product: Product) {}
+}
+
+export class AddProductFailed {
+  static readonly type = '[Product] Add Product Failed';
+  constructor(public message: string) {}
+}
+
+export class AddProductReset {
+  static readonly type = '[Product] Add Product Reset';
+}
+
