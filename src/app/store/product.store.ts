2f62511846ff3ed8d2c5f3756a99b01dd1a8b97b
Milton Arenas <hefziben@gmail.com>
2023-12-22 12:37:08 -0500

    F4 - Add product - added selectors and effects

--- src/app/store/product.store.ts
+++ src/app/store/product.store.ts
@@ -3,7 +3,7 @@
 import { State, Action, StateContext, Selector, Store } from '@ngxs/store';
 import { tap } from 'rxjs/operators';
 import { ProductService } from '../services/product.service';
-import {GetProducts, GetProductsFailed, GetProductsReset, GetProductsSuccess, ProductStateModel } from './product.actions';
+import {GetProducts, GetProductsFailed, GetProductsReset, GetProductsSuccess, ProductStateModel, ValidateProductId, ValidateProductIdFailed, ValidateProductIdSuccess, AddProductReset, AddProduct, AddProductSuccess, AddProductFailed } from './product.actions';
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product.model';
 
@@ -14,6 +14,9 @@ import { Product } from '../models/product.model';
     products: [],
     loadingProducts: false,
     loadingProductsError: null,
+    validatingProductId: false,
+    productIdExist: false,
+    addingProduct: false
   },
 })
 
@@ -25,6 +28,16 @@ export class ProductState {
   @Selector()
   static getProducts(state: ProductStateModel): Product[] {
     return state.products;
+  }
+
+  @Selector()
+  static isProductIdExisting(state: ProductStateModel): boolean {
+    return state.productIdExist;
+  }
+
+  @Selector()
+  static validatingProductId(state: ProductStateModel): boolean {
+    return state.validatingProductId;
   }
 
   // Actions
@@ -66,7 +79,84 @@ export class ProductState {
       patchState({
         loadingProducts: false,
         loadingProductsError: null,
-        products: []
+        products: [],
+        validatingProductId: false,
+        productIdExist: false
+      });
+  }
+
+  @Action(ValidateProductId)
+  validateProductId(
+      context: StateContext<ProductStateModel>,
+      payload: ValidateProductId) {
+      return this.productService.validateProductId(payload.ProductId).pipe(tap(response => {
+        context.patchState({ validatingProductId: true});
+
+        this.store.dispatch(new ValidateProductIdSuccess(response));
+      }, error => {
+        this.store.dispatch(new ValidateProductIdFailed(error));
+    }));
+  }
+
+  @Action(ValidateProductIdSuccess)
+  validateProductIdSuccess(
+      context: StateContext<ProductStateModel>,
+      payload: ValidateProductIdSuccess) {
+      context.patchState({
+          validatingProductId: false,
+          productIdExist: payload.productIdExist
+      });
+  }
+
+  @Action(ValidateProductIdFailed)
+  validateProductIdFailed(
+      { patchState }: StateContext<ProductStateModel>) {
+      patchState({
+        validatingProductId: false
+      });
+  }
+
+
+  @Action(AddProduct)
+  dddProduct(
+      context: StateContext<ProductStateModel>,
+      payload: AddProduct) {
+      return this.productService.addProduct(payload.product).pipe(tap(response => {
+        context.patchState({ addingProduct: true});
+
+        this.store.dispatch(new AddProductSuccess(response));
+      }, error => {
+        this.store.dispatch(new AddProductFailed(error));
+    }));
+  }
+
+  @Action(AddProductSuccess)
+  addProductSuccess(
+      context: StateContext<ProductStateModel>,
+      payload: AddProductSuccess) {
+        const state = { ...context.getState() };
+      context.patchState({
+          products: [...state.products, payload.product],
+          addingProduct: false
+      });
+  }
+
+  @Action(AddProductFailed)
+  addProductFailed(
+      { patchState }: StateContext<ProductStateModel>,
+      payload: AddProductFailed) {
+      patchState({
+        addingProduct: false,
+        loadingProductsError: payload.message
+      });
+  }
+
+  @Action(AddProductReset)
+  addProductsReset(
+      { patchState }: StateContext<ProductStateModel>) {
+      patchState({
+        validatingProductId: false,
+        productIdExist: false,
       });
   }
  }
