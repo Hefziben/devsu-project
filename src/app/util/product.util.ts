4bf373c5a3436cdb4b817d34dcb5cd3dc868e671
Milton Arenas <hefziben@gmail.com>
2023-12-22 12:39:14 -0500

    F4 - Add product - added getDefaultProduct util

--- src/app/util/product.util.ts
+++ src/app/util/product.util.ts
@@ -1,5 +1,6 @@
 import { Observable, of } from "rxjs";
 import { Product } from "../models/product.model";
+import { addYearsToDate, getCurrentDate } from "./date.util";
 
 export const filterItems = (searchTerm: string, products: Product[]): Observable<Product[]> => {
   if (searchTerm) {
@@ -11,3 +12,15 @@ export const filterItems = (searchTerm: string, products: Product[]): Observable
     return of(products);
   }
 }
+
+export const getDefaultProduct = (): Product => {
+  const currentDate = getCurrentDate();
+  return {
+    id: '',
+    name: '',
+    description: '',
+    logo: '',
+    date_release: currentDate,
+    date_revision: addYearsToDate(currentDate)
+  };
+};
