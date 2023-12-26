5bf4000d95c3c4e9ab146ad447a6113d17fbd616
Milton Arenas <hefziben@gmail.com>
2023-12-26 14:52:14 -0500

    updated share module

--- src/app/shared/share.module.ts
+++ src/app/shared/share.module.ts
@@ -2,18 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './layout/header/header.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
+import { LoadingComponent } from './ui/loading/loading.component';
+import { ErrorPopupComponent } from './ui/error-modal/error-modal.component';
+import { ConfirmationModalComponent } from './ui/confirmation-modal/confirmation-modal.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     PaginationComponent,
+    LoadingComponent,
+    ErrorPopupComponent,
+    ConfirmationModalComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    PaginationComponent
+    PaginationComponent,
+    LoadingComponent,
+    ErrorPopupComponent,
+    ConfirmationModalComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
