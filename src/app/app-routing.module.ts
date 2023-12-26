cbe075a7deaca7c66421a676eebfb44aeb74648e
Milton Arenas <hefziben@gmail.com>
2023-12-26 14:48:10 -0500

    cleanup and refactor app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
@@ -9,12 +9,13 @@ const routes: Routes = [
   },
   {
     path: 'products',
-    loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
+    loadChildren: () => import('./product/product.module').then(m => m.ProductModule),
+    data: { preload: true }
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
